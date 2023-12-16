af61604115d894335fcbc040d69dc735f72ce3bd
lvdaojing <152788417+lvdaojing@users.noreply.github.com>
2023-12-16 20:14:16 +0800

    1234

--- src/Algoriscope/color.h
+++ src/Algoriscope/color.h
@@ -1,11 +1,37 @@
-#ifndef COLOR_H
-#define COLOR_H
+#ifndef ALGORISCOPE_VECTOR2_H
+#define ALGORISCOPE_VECTOR2_H
 
-class color {
-	public:
+namespace Algoriscope {
 
-	private:
-		int r, g, b, a;
-};
+
+	class Color {
+		public:
+			int r, g, b;
+			float a, h, s, l;
+			Color(const char* RGB);
+			Color(int r = 0, int g = 0, int b = 0, float a = 1.0);
+			Color(float h=0, float s=0, float l=0);
+			void SetRed(int r) {
+				this->r = r;
+			};
+			void SetGreen(int g) {
+				this->g = g;
+			};
+			void SetBlue(int b) {
+				this->b = b;
+			};
+			void SetA(float a) {
+				this->a = a;
+			};
+		private:
+			void Check1();
+			void Check2();
+			void SwitchfromRGBtoHSL();
+			void SwitchfromHSLtoRGB();
+
+
+	};
+	
+}
 
 #endif
