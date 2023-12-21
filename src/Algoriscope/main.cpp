75689e5c76fe05219bb494987a661fb31974aca2
Cluster-YW <linziqwq@qq.com>
2023-12-21 14:31:00 +0800

    Dynamics的合并测试

--- src/Algoriscope/main.cpp
+++ src/Algoriscope/main.cpp
@@ -10,26 +10,40 @@ int main() {
 	int t = 0;
 	clock_t last, now;
 	last = now = clock();
-	Dynamics d(0,5.0f,0.25f,1.0f);
+	Dynamics d(0, 5.0f, 0.25f, 1.0f);
+	Dynamics d2(0, 3.0f, 1.0f, 0.5f);
 	Color color("#66CCFF");
-	Vector2 a(0,0);
+	Vector2 a(0, 0);
 	while (1) {
 		now = clock();
 		if (now - last > CLOCKS_PER_SEC / FPS) {
-			if(t/((int)FPS*1)%2){
-				d=1.0f;
-				color=Color("#66CCFF");
+			if (t / ((int)FPS * 1) % 2) {
+				d = 1.0f;
+				color = Color("#66CCFF");
+			} else {
+				d = 0.0f;
+				color = Color("#FF0000");
 			}
-			else{
-				d=0.0f;
-				color=Color("#FF0000");
+
+			if (t % (int)(FPS * 0.5f) == 0) {
+				auto temp = d2() + 0.25f;
+				if (temp<0.5f) {
+					d2=temp;
+				}
+				else{
+					d2=-0.5f;
+				}
 			}
-			render.drawLine(a, Vector2(0.5f,d()), color);
-			t+=1;
-			
-			d.update(1.0/FPS);
+
+
+			render.drawLine(a, Vector2(0.5f, d()), color);
+			render.drawRect(Vector2(-0.5f, -d2()), Vector2(0.2f, 0.2f), color);
+			t += 1;
+
+			d.update(1.0 / FPS);
+			d2.update(1.0 / FPS);
 			render.update();
-			
+
 			last = now;
 		}
 	}
