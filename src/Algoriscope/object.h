7a4501a9bf7edca06afc7146440d68a8a2e8ed02
totop0san <152789279+totop0san@users.noreply.github.com>
2023-12-23 20:55:43 +0800

    commit

--- src/Algoriscope/object.h
+++ src/Algoriscope/object.h
@@ -1,12 +1,33 @@
 #ifndef OBJECT_H
 #define OBJECT_H
 
+#include"vector2.h"
+#include"color.h"
+#include"render.h"
+
 namespace Algoriscope {
-	class Object {
+	class ObjectRect {
 		public:
+			void getRect(const Vector2& x, const Vector2& y, const Color& z){
+				{
+					pos=x;size=y;col=z;
+				}
+			}
+			void Rect();
 		private:
+			Vector2 pos;
+			Vector2 size;
+			Color col;
+		
 
 	};
+	void ObjectRect::Rect(){
+		{
+			Render drawRect(pos,size,col);
+		}
+	}
+
+	
 }
 
 #endif
