f4b025c2e9b1ddb09602f6e573fe174bedb71f28
Cluster-YW <linziqwq@qq.com>
2024-02-02 17:21:33 +0800

    Revert "object"
    This reverts commit e73e2aa698959d196455ea48acd17d5df4ead102.

--- src/Algoriscope/main.cpp
+++ src/Algoriscope/main.cpp
@@ -1,6 +1,5 @@
 // Std. Includes
 #include "render.h"
-<<<<<<< Updated upstream
 #include <iostream>
 #include <map>
 #include <string>
@@ -67,19 +66,4 @@ int main() {
 		
 		t += 1.0f / FPS;
 	}	
-=======
-#include"object.h"
-#include<iostream>
-using namespace Algoriscope;
-int main() {
-	Render render;
-	int arr[10]={1,2,3,4,5,6,7,8,9,10};
-	Object a(arr,10);
-	while(1){
-		a.DrawBar(render);
-		render.update();
-	}
-	
-
->>>>>>> Stashed changes
 }
--- src/Algoriscope/object.h
+++ src/Algoriscope/object.h
@@ -1,25 +1,33 @@
 #ifndef OBJECT_H
 #define OBJECT_H
+
+#include"vector2.h"
+#include"color.h"
 #include"render.h"
-#include<vector>
 
 namespace Algoriscope {
-	class Object {
-	public:
-		vector<int>a;
-		int n=0;
-		Vector2 pos{Vector2 (0,-0.5)};//原点
-		Vector2 size{Vector2(0.05,0)};//
-		Vector2 unit{Vector2(0.05,0.05)};//单位
-		Color col[2]{("#FF0000"),("#00FF00")};
-		//定义数据，arr为数组名，n为数据个数
-		Object(int*arr,int n);
-		Object(int x);
-		int set(int*arr,int n);
-		int DrawObject(Render&render);
-		int DrawBar(Render&render);
+	class ObjectRect {
+		public:
+			void getRect(const Vector2& x, const Vector2& y, const Color& z){
+				{
+					pos=x;size=y;col=z;
+				}
+			}
+			void Rect();
+		private:
+			Vector2 pos;
+			Vector2 size;
+			Color col;
 		
-	};	
+
+	};
+	void ObjectRect::Rect(){
+		{
+			Render drawRect(pos,size,col);
+		}
+	}
+
+	
 }
 
 #endif
