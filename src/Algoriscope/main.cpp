6e931c86381b99dd557920434afc805e7adcfa56
Cluster-YW <linziqwq@qq.com>
2024-03-18 16:04:03 +0800

    小修改和测试

--- src/Algoriscope/main.cpp
+++ src/Algoriscope/main.cpp
@@ -59,45 +59,45 @@ int main() {
 	Scene scn(1920, 1080, 100);
 //	scn.debug_function = LOOP;
 	int n = 9;
-	auto Bs = new BarArray(Vector2(0, -60.0f), n, 50,
-	                       100, 100); // 输入起码包括长度+绑定指针
-	scn.addObject(*Bs);
 	n = 10;
 	float array[n] = {0.3f, -0.6f, -0.7f, 0.2f, 0.8f
 	                  , 0.1f, 0.9f, 0.4f, 2.0f, 1.0f
 	                 };
+
+	auto Bs = new BarArray(Vector2(0, -60.0f), n, 50,
+	                       100, 100); // 创建一个条形图对象
+	scn.addObject(*Bs);
 	Bs->setBind(array, n);
-	Bs->setAlign("right");
-	Bs->autoScale(500);
+	Bs->autoScale(500); // 进行相关设置
 	Bs->setCallBack(make);
-	Bs->setBarsCallBack(hightlight);
-	auto Title = new Text("冒泡排序演示：",Vector2(-800,400),15,"white");
-	auto Subtitle = new Text("此时进行：",Vector2(-800,300),10,"white","ld");
+	Bs->setBarsCallBack(hightlight); // 用于交互的回调函数
+	auto Title = new Text("冒泡排序演示：", Vector2(-800, 400), 15, "white");
+	auto Subtitle = new Text("此时进行：", Vector2(-800, 300), 10, "white", "ld");
 	scn.addObject(*Title);
-	scn.addObject(*Subtitle);
+	scn.addObject(*Subtitle); // 加入两行文字
+
+
 	for (int i = n - 1; i >= 0; i--) {
 		for (int j = 0; j < i; j++) {
-			Bs->getBar(j)->setDefaultColor("blue");
-			Bs->getBar(j+1)->setDefaultColor("blue");
-			Bs->getBar(j)->resetColor();
-			Bs->getBar(j+1)->resetColor();
-			Subtitle->setContent("比较");
-			scn.run(1000);
-			Subtitle->setContent("无需交换");
+			Bs->setDefaultColor("blue", j, j + 1);
+			Bs->resetColor(j, j + 1); // 修改 j 和 j+1 条形的颜色为蓝色
+			Subtitle->setContent("比较"); //修改文字内容
+			scn.run(1000); // 运行动画 1s
+
+			Subtitle->setContent("无需交换");// 修改文字内容
+
 			if (array[j] < array[j + 1]) {
 				swap(array[j], array[j + 1]);
 
-				Bs->animSwap(j, j + 1);
-			Subtitle->setContent("进行交换！");
+				Bs->animSwap(j, j + 1);// 执行交换动画
+				Subtitle->setContent("进行交换！");// 修改文字内容
 			}
-			scn.run(1000);
-			Bs->getBar(j)->setDefaultColor("red");
-			Bs->getBar(j+1)->setDefaultColor("red");
-			Bs->getBar(j)->resetColor();
-			Bs->getBar(j+1)->resetColor();
+			scn.run(1000); // 运行动画 1s
+			Bs->setDefaultColor("red", j, j + 1);
+			Bs->resetColor(j, j + 1); // 修改 j 和 j+1 条形的颜色回红色
 		}
-		Bs->getBar(i)->setDefaultColor("green");
-		Bs->getBar(i)->resetColor();
+		Bs->setDefaultColor("green", i); // 将排序好的数据颜色设置为绿色
+		Bs->resetColor(i);
 	}
 	scn.run(5000);
 }
--- src/Algoriscope/object.cpp
+++ src/Algoriscope/object.cpp
@@ -304,16 +304,18 @@ namespace Algoriscope {
 		for (i = i; i <= j; i++) {
 			bars[i]->resetColor();
 		}
-	};void BarArray::setDefaultColor(Color in, int _i) {
+	};
+	void BarArray::setDefaultColor(Color in, int _i) {
 		bars[_i]->setDefaultColor(in);
 		resetColor(_i);
 	};
 	void BarArray::setDefaultColor(Color in, int _i, int _j) {
-		int i = _i > _j ? _i : _j;
+		int i = _i < _j ? _i : _j;
 		int j = _i > _j ? _i : _j;
-		
-		for (i = i; i <= j; i++)setDefaultColor(in,i);
-		resetColor(i,j);
+
+		for (i = i; i <= j; i++)
+			setDefaultColor(in, i);
+		resetColor(i, j);
 	};
 }
 
